48281fae883f48ca1cd457eb944de343babf0e18
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2014-01-19 18:39:50 +0800

    Updated meta procedure.

--- meta.js
+++ meta.js
@@ -9,14 +9,26 @@
 
 	@module-configuration:
 		{
-			"moduleName": "ocfParser",
+			"moduleName": "meta",
 			"authorName": "Richeve S. Bebedor",
 			"isGlobal": true
 		}
 	@end-module-configuration
 
 	@module-documentation:
-		The meta function dictates strict rule
+		The meta function dictates strict rule in checking and applying
+			meta configurations.
+
+		It basically reads a simple rule configuration.
+			{
+				"ruleKey[:optional]": type[-type][|type...][:defaultValue][~linkedProperty] 
+			}
+
+			~ linked property *	 	priority 0
+			: default value *	 	priority 0
+			- group of types		priority 1
+			| or separator		  	priority 2
+			; meta mutator          priority 3
 	@end-module-documentation
 */
 meta = function meta( method, parameters, configuration ){
@@ -40,4 +52,32 @@ meta = function meta( method, parameters, configuration ){
 	if( meta.parameters.isArguments( ) ){
 		parameters = argumentsToArray( parameters );
 	}
+
+	/*
+		isEmpty
+		isNotEmpty
+		isExisting
+		isNotExisting
+		isNumber
+		isNotNumber
+		isObject
+		isNotObject
+		isArray
+		isNotArray
+		isString
+		isNotString
+		isBoolean
+		isNotBoolean
+	*/
+	var flags = {
+		"emptyFlag": false,
+		"existingFlag": false,
+		"numberFlag": false,
+		"objectFlag": false,
+		"arrayFlag": false,
+		"stringFlag": false,
+		"booleanFlag": false,
+		"optionalFlag": false
+	}
+	
 };
